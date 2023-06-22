07d6c7aa15bf1b52dd6674ce9b580e55d41f1f3b
Dzmitry Shiwe <demiyan888@yandex.ru>
2023-06-22 13:34:56 +0300

    feat: implement function compress

---
+++ src/compress.js
@@ -0,0 +1,17 @@
+import { createBrotliCompress } from 'node:zlib';
+import { createReadStream, createWriteStream } from "node:fs";
+import { absPath } from './utils/fs';
+
+export const compress = async (workDir, inputDir, destDir) => {
+  try {
+    const gzip = createBrotliCompress();
+    const readerStream = createReadStream(await absPath(workDir, inputDir));
+    const writerStream = createWriteStream(await absPath(workDir, destDir));
+
+    readerStream.pipe(gzip).pipe(writerStream);
+  } catch (error) {
+    console.log('Operation failed');
+  }
+
+
+}
