81e79a21c2cf8417932cd30011920bb17f7dbd02
Dzmitry Shiwe <demiyan888@yandex.ru>
2023-06-22 13:39:21 +0300

    feat: implement function decompress

---
+++ src/decompress.js
@@ -0,0 +1,17 @@
+import { createBrotliDecompress } from 'node:zlib';
+import { createReadStream, createWriteStream } from "node:fs";
+import { absPath } from './utils/fs';
+
+export const decompress = async (workDir, inputDir, destDir) => {
+  try {
+    const uzip = createBrotliDecompress();
+    const readerStream = createReadStream(await absPath(workDir, inputDir));
+    const writerStream = createWriteStream(await absPath(workDir, destDir));
+
+    readerStream.pipe(uzip).pipe(writerStream);
+  } catch (error) {
+    console.log('Operation failed');
+  }
+
+
+}
