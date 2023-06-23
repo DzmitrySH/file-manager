c03dc1d6110919d0ea9e6a17cde4ecd0cb9bd92c
Dzmitry Shiwe <demiyan888@yandex.ru>
2023-06-23 12:30:38 +0300

    feat: implement function ls

---
+++ src/ls.js
@@ -0,0 +1,15 @@
+import { readdir } from "fs/promises";
+
+export const ls = async (currentDir) => {
+  let entries = await readdir(currentDir, { withFileTypes: true });
+  let dirsList = [];
+  let filesList = [];
+
+  for (let entry of entries)
+      if (entry.isFile())
+          filesList.push({"Name": entry[""], "Type": "file"});
+      else if (entry.isDirectory())
+          dirsList.push({"Name": entry[""], "Type": "directory"});
+  const compareTo = (a, b) => a.Name.localeCompare(b.Name);
+  return dirsList.sort(compareTo).concat(filesList.sort(compareTo));
+};
