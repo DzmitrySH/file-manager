ae484b075b019b7a7245fd484102fc3f76389cf7
Dzmitry Shiwe <demiyan888@yandex.ru>
2023-06-23 13:52:47 +0300

    feat: add function argument

---
+++ src/utils/args.js
@@ -0,0 +1,10 @@
+export const argumentArgs = () => {
+  const args = {};
+
+  for (let i = 0; i < process.argv.length; i++)
+      if (process.argv[i].startsWith('--')) {
+          let [key, value] = process.argv[i].replace('--', '').split('=');
+          args[key] = value;
+      }
+  return args;
+};alue
