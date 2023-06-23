e28612cb079386767128aa67687bdfa6ed3b7a47
Dzmitry Shiwe <demiyan888@yandex.ru>
2023-06-23 11:43:44 +0300

    feat: add constant file

---
+++ src/utils/const.js
@@ -0,0 +1,9 @@
+
+export const usernameDefault = "Username"
+
+export const messages = {
+  welcomUser: `Welcome to the File Manager, ${username}!`,
+  exitUser: `Thank you for using File Manager, ${username}, goodbye!`,
+  invalidMessage: 'Invalid input',
+  failedMessage: 'Operation failed'
+}
