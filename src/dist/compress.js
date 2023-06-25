8ddf55fe76d424148eab2f7bbe13b526828d3891
Dzmitry Shiwe <demiyan888@yandex.ru>
2023-06-25 20:10:48 +0300

    fix: bugfix brotli arhive

--- src/dist/compress.js
+++ src/dist/compress.js
@@ -1,13 +1,21 @@
-import { createBrotliCompress } from 'node:zlib';
+import { createBrotliCompress } from "node:zlib";
 import { createReadStream, createWriteStream } from "node:fs";
+import { resolve, parse } from "path";
+import { pipeline } from "stream/promises";
 import { absPath } from "../utils/fs.js";
 
 export const compress = async (workDir, inputDir, destDir) => {
   try {
+    const inDirectory = absPath(workDir, inputDir);
+    const oldDirectory = absPath(workDir, destDir);
+    const pathFileIn = resolve(inDirectory);
+    const { base } = parse(pathFileIn);
+    const fileName = `${base}.br`;
+    const pathFileOut = resolve(oldDirectory, fileName);
     const gzip = createBrotliCompress();
-    const readerStream = createReadStream(await absPath(workDir, inputDir));
-    const writerStream = createWriteStream(await absPath(workDir, destDir));
-    readerStream.pipe(gzip).pipe(writerStream);
+    const readerStream = createReadStream(pathFileIn, "utf8");
+    const writerStream = createWriteStream(pathFileOut, { flags: "wx" });
+    await pipeline(readerStream, gzip, writerStream);
   } catch (error) {
       return { error: error };
   }
--- src/dist/decompress.js
+++ src/dist/decompress.js
@@ -1,14 +1,21 @@
 import { createBrotliDecompress } from "node:zlib";
 import { createReadStream, createWriteStream } from "node:fs";
+import { resolve, parse } from "path";
+import { pipeline } from "stream/promises";
 import { absPath } from "../utils/fs.js";
 
 export const decompress = async (workDir, inputDir, destDir) => {
   try {
+    const inDirectory = absPath(workDir, inputDir);
+    const oldDirectory = absPath(workDir, destDir);
+    const pathFileIn = resolve(inDirectory);
+    const { name } = parse(pathFileIn);
+    const pathFileOut = resolve(oldDirectory, name);
     const uzip = createBrotliDecompress();
-    const readerStream = createReadStream(await absPath(workDir, inputDir));
-    const writerStream = createWriteStream(await absPath(workDir, destDir));
-    readerStream.pipe(uzip).pipe(writerStream);
+    const readerStream = createReadStream(pathFileIn);
+    const writerStream = createWriteStream(pathFileOut, { flags: "w+" });
+    await pipeline(readerStream, uzip, writerStream);
   } catch (error) {
-
+    return { error: error };
   }
 }
