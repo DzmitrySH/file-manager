1c67c98126265785859e1066d5d23363eab49cd4
Dzmitry Shiwe <demiyan888@yandex.ru>
2023-06-25 13:55:17 +0300

    fix: current directiry function cd

--- src/dist/cd.js
+++ src/dist/cd.js
@@ -1,9 +1,13 @@
 import { stat } from "fs/promises";
 import { absPath } from "../utils/fs.js";
-import { error } from "console";
 
 export const cd = async (currDir, newDir) => {
-    const path = absPath(currDir, newDir);
-    const directory = (await stat(path)).isDirectory();
-    return directory ? {currDir: path} : {error: new Error(error)};
+    try {
+        const path = absPath(currDir, newDir);
+        const directory = (await stat(path)).isDirectory();
+        if (directory)
+           return {currDir: path}
+    } catch (error) {
+       return {error: error}
+    }
 };
--- src/dist/move.js
+++ src/dist/move.js
@@ -1,44 +1,27 @@
 import { createReadStream, createWriteStream } from "node:fs";
 import { absPath } from "../utils/fs.js";
-import { stat, rm } from "fs/promises";
-import path from "path";
+import { rm } from "fs/promises";
 
 export const move = async (workDir, inputDir, destDir) => {
-  const absolutInput = absPath(workDir, inputDir);
-  let absolutDest = absPath(workDir, destDir);
-
-  let pathStat = await stat(absolutDest);
-  if (!pathStat.isDirectory()) throw new Error();
-  absolutDest = path.join(absolutDest, path.basename(src));
-
-  const readStream = createReadStream(absolutInput, "utf8");
-  await new Promise((res, rej) => {
-    readStream.on("open", res);
-    readStream.on("error", rej);
-  });
-
-  const writeStream = createWriteStream(absolutDest, { flags: "wx" });
-  await new Promise((res, rej) => {
-    writeStream.on("open", res);
-    writeStream.on("error", rej);
-  });
-
-  readStream.on("data", (chunk) => {
-    writeStream.write(chunk);
-  });
-
-  await new Promise((res, rej) => {
-    readStream.on("end", () => {
-      writeStream.destroy();
-      res();
-    });
-  });
-
-  const dirPath = absPath(workDir, inputDir);
   try {
-    await rm(dirPath);
+    const absolutInput = await absPath(workDir, inputDir);
+    const absolutDest = await absPath(workDir, destDir);
+    const readStream = createReadStream(absolutInput, "utf8");
+    const writeStream = createWriteStream(absolutDest, { flags: "wx" });
+    readStream.on("error", () => {
+      console.log("Operation failed");
+    });
+    writeStream.on("error", () => {
+      console.log("Operation failed");
+    });
+    try {
+      readStream.pipe(writeStream).on("close", async () => {
+        await rm(absolutInput);
+      });
+    } catch (error) {
+      return { error: error };
+    }
   } catch (error) {
-    if (error.code == "EEXIST") throw Error("FS operation failed");
-    else throw error;
+    return { error: error };
   }
 };
